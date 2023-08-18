e4259c1d97535e31871a3e8c54637668e3c88fc4
Ivan <ButorinLP@yandex.ru>
2023-08-18 21:02:42 +0300

    limiterUpdate

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: 'Too many requests created from this IP, please try again after 10 min',
   standardHeaders: true,
   legacyHeaders: false,
